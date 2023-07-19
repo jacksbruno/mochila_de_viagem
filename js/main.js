8059bf13930f65a5e807234ba86085ae78aa9e24
Jacks Bruno Moran Prado <jacks.prado@grupopereira.local>
2023-07-19 15:39:18 -0400

    implementando localstorage

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 const form = document.querySelector('[data-form]')
 const lista = document.querySelector('ul.lista')
+const nome = document.querySelector('#nome')
+const quantidade = document.querySelector('#quantidade')
+const itens = JSON.parse(localStorage.getItem('itens')) || []
+
+itens.forEach(item => {
+  lista.appendChild(criaElemento(item))
+})
 
 form.addEventListener('submit', (evento) => {
   evento.preventDefault()
-  const nome = evento.target.elements['nome'].value
-  const quantidade = evento.target.elements['quantidade'].value
-  lista.appendChild(criaElemento(nome, quantidade))  
+  const item = {
+    nome: nome.value,
+    quantidade: quantidade.value
+  }
+
+  const existe = itens.find(e => e.nome === nome.value)
+  if(existe){}
+
+  lista.appendChild(criaElemento(item))
+  itens.push(item)
+  localStorage.setItem('itens', JSON.stringify(itens))
+  nome.value = ''
+  quantidade.value = ''
 })
 
-function criaElemento(nome, quantidade){
+function criaElemento(item){
   const linha = document.createElement('li')
   linha.className = 'item'
-  const conteudo = `<strong>${quantidade}</strong>${nome}`
+  const conteudo = `<strong>${item.quantidade}</strong>${item.nome}`
   linha.innerHTML = conteudo
 
   return linha
